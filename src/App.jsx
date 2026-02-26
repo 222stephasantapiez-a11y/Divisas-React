7d42784ad93c6283730f894cd5804af8ca487897
stephanie <222stephasantapiez@gmail.com>
2026-02-26 08:49:10 -0500

    unos commits

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import {useEffect, useState} from 'react'
+
+const App = () => {
+  const [divisas, setDivisas] = useState([])
+
+  useEffect(() => {
+   alert("se al redenrizar o al cambiar el valor de divisas")
+  }, [])
+
+  const consultar = async () => {
+    let url = "https://co.dolarapi.com/v1/cotizaciones"
+
+     const resultado = await fetch(url)
+     return resultado.json()
+  }
+  
+  return (
+    <>  <div className="contenedor">
+        <h1>Convertir desde COP</h1>
+        <input type="number" id="valor" placeholder="Cantidad en pesos (COP)"/>
+        
+        <select id="opcionesDivisas">
+            <option value="">Cargando divisas...</option>
+        </select>
+
+        <button >Convertir</button>
+        <p id="resultado"></p>
+    </div>
+</>
+  )
+}
+
+export default App
